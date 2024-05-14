b5671b5e1d28a3a410269ab95c9918dd3cea7bcb
dave-eras <d.mitchell@cll.be>
2024-05-14 15:39:16 +0200

    etc.

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
-import { main } from './assistant.js';
+import { main } from './assistant2.js';
 
 const app = express();
 
